52425b835f34750845856daa5301190ea663e468
KhatiRaj <93217940+Klsuthar@users.noreply.github.com>
2026-05-10 00:14:04 +0530

    65

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ document.addEventListener('DOMContentLoaded', () => {
             id: "cnc-work",
             displayName: "CNC Work",
             folderName: "CNC_Work",
-            imageFiles: ["1.jpg"]
+            imageFiles: ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg", "8.jpeg", "9.jpeg", "10.jpeg", "11.jpeg", "12.jpeg", "13.jpeg", "14.jpeg"]
         },
         {
             id: "bed",
